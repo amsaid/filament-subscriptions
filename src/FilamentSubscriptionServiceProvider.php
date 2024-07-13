0df411a47411fe21adf34cebdc4f2288dd9775c1
Ecoleplus <743038-ecoleplus@users.noreply.gitlab.com>
2024-07-13 16:47:01 +0100

    fixes

---
+++ src/FilamentSubscriptionServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace EcolePlus\FilamentSubscription;
+
+use BladeUI\Icons\Factory;
+use EcolePlus\FilamentSubscription\Commands\FilamentSubscriptionCommand;
+use Filament\Support\Assets\Css;
+use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Contracts\Container\Container;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class FilamentSubscriptionServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('filament-subscriptions')
+            ->hasTranslations()
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasMigration('create_filament_subscriptions_table')
+            ->hasCommand(FilamentSubscriptionCommand::class);
+    }
+
+    public function packageBooted(): void
+    {
+        FilamentAsset::register(
+            [
+                Css::make(
+                    'filament-subscriptions-tailwindcss-styles',
+                    __DIR__.'/../dist/css/style.css'
+                ),
+            ],
+            'filament-subscriptions'
+        );
+    }
+
+    public function packageRegistered(): void
+    {
+        $this->callAfterResolving(Factory::class, function (
+            Factory $factory,
+            Container $container
+        ) {
+            $config = $container->make('config')->get('blade-heroicons', []);
+
+            $factory->add(
+                "ecoleplus-subs-icon",
+                array_merge(["path" => __DIR__ . "/../resources/svg"], $config)
+            );
+        });
+    }
+}
